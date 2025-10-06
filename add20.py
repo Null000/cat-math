639e246f80bf79dc92f5bd0cde8c257f8fc4f5cd
Damjan Košir <damjan.kosir@gmail.com>
2025-10-06 20:22:40 +0200

    switching to node

--- add20.py
+++
@@ -1,9 +0,0 @@
-def generate_all_addition_problems():
-    """Generate all possible addition problems (1-20 range)"""
-    problems = []
-    for num1 in range(1, 11):
-        for num2 in range(1, 11):
-            problem = f"{num1} + {num2} = "
-            correct_answer = num1 + num2
-            problems.append((problem, correct_answer))
-    return problems
