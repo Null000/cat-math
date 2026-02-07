a3c424acd1eb86166a3c1355279903bebc015d51
Damjan Košir <damjan.kosir@gmail.com>
2026-02-07 15:57:35 +0100

    better deploy script

--- scripts/pages.sh
+++ scripts/pages.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Error: There are uncommitted changes. Commit or stash them first."
+  exit 1
+fi
+
 bun run build
 
-rm -r /tmp/cat-math
+rm -rf /tmp/cat-math
 cp -r dist /tmp/cat-math
 
 git switch pages
 
-rm -r *
+git rm -rf .
 cp -r /tmp/cat-math/* .
+
+git add -A
+git commit -m "deploy $(date -Iseconds)"
+git push
+git switch main
